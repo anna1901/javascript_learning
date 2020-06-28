9b5631036ed62f9151b17d7aad2aa9df72395e94
Anna Olak <anna.olak1901@gmail.com>
2020-06-29 01:27:32 +0200

    Create playRound function

--- rock_paper_scissors/scripts/script.js
+++ rock_paper_scissors/scripts/script.js
@@ -3,4 +3,32 @@ function computerPlay() {
   return options[Math.floor(Math.random() * 3)];
 }
 
-console.log(computerPlay())
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+  if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      alert("You win! Paper beats Rock");
+    } else if (computerSelection === "scissors") {
+      alert("You lose! Scissors beat Paper");
+    } else {
+      alert("It's a tie!");
+    }
+  } else if (playerSelection === "rock"){
+    if (computerSelection === "rock") {
+      alert("It's a tie!");
+    } else if (computerSelection === "scissors") {
+      alert("You win! Rock beat Scisors");
+    } else {
+      alert("You lose! Paper beat Rock");
+    }
+  } else {
+    if (computerSelection === "rock") {
+      alert("You lose! Rock beat scissors");
+    } else if (computerSelection === "scissors") {
+      alert("It's a tie!");
+    } else {
+      alert("You win! Scissors beat Paper");
+    }
+  }
+}
